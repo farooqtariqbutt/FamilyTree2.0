1e347ced06c854a15707e78cfffa1a1beb9d552f
farooqtariqbutt <farooqtariqbutt@gmail.com>
2025-08-18 02:14:55 +0500

    Update Input.tsx

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  label: string;
+  label: React.ReactNode;
 }
 
 const Input: React.FC<InputProps> = ({ label, ...props }) => {
